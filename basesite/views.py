3675282f6d22139fa2cac191ec1beb1290dcda71
Dan Selden <danselden@me.com>
2012-04-01 12:02:27 -0400

    Latest

--- basesite/views.py
+++ basesite/views.py
@@ -2,10 +2,11 @@
 from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
-    template_name = 'home.html'
+    template_name = 'basesite/home.html'
 
 class About(TemplateView):
-    template_name = 'about.html'
+    template_name = 'basesite/about.html'
 
 class Contact(TemplateView):
-    template_name = 'contact.html'
+    template_name = 'basesite/sitecontact.html'
+
