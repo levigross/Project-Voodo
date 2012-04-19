d5f3add2e0c6465b772c5649887fc927695dcc21
Navit Keren <navitush@gmail.com>
2012-04-19 14:56:20 -0400

    models for sigunppage

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Signup(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    pw = models.CharField(_('password'), max_length=8, help_text=_("Please enter maximun 8 characters"))
+    is_accepted = models.BooleanField(default=False)
+    admin_comments = models.TextField(blank=True, null=True)
+
+    def __unicode__(self):
+    	return self.name
+
+
+
+
