1cf52025f0b417097013533d354423da5d082525
Dan Selden <danselden@me.com>
2012-04-22 12:02:36 -0400

    BaseTemplateHTML

--- voodo/settings.py
+++ voodo/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '%s/static/' % os.getcwdu()
+STATIC_ROOT = '%s/static_files/' % os.getcwdu()
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -68,9 +68,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+	u'%s/static/' % os.getcwdu(),
 )
 
 # List of finder classes that know how to find static files in
