cca06a8edfe7689ad484786196929e73880fa1ab
Tamara <tamaramakarem@gmail.com>
2012-04-22 11:16:30 -0400

    updates

--- signup/models.py
+++ signup/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-<<<<<<< HEAD
 
 class Signup(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    pw = models.CharField(_('password'), max_length=8, help_text=_("Please enter maximun 8 characters"))
+    pw = models.CharField(('password'), max_length=8, help_text=("Please enter maximun 8 characters"))
     is_accepted = models.BooleanField(default=False)
     admin_comments = models.TextField(blank=True, null=True)
 
