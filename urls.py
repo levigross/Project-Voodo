f6b0b5bc6f4b3959207c015f8d7c989eb5804383
Tamara <tamaramakarem@gmail.com>
2012-04-22 13:30:17 -0400

    files deleted

--- urls.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: UTF-8 -*-
-from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('',
-	url(r'^$', 'login.views.home', name='home'),
-	url(r'^login/', include('login.urls')),
-)
