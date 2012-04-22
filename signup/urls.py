a3f6394f0f81f3a17d0f1256b328da203761f566
Tamara <tamaramakarem@gmail.com>
2012-04-22 12:53:52 -0400

    urls and templates fixed

--- signup/urls.py
+++ signup/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, url
 from signup.views import SiteSignup
 
 urlpatterns = patterns('',
-    url(r'^$', SiteSignup.as_view(), name='site_signup'),
+    url(r'^/signup', SiteSignup.as_view(), name='site-signup'),
 )
