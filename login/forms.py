972f9eb677db2602fb98a5860d9a8a00356ab73b
kbarr <kierstie.barr@gmail.com>
2012-04-22 12:54:46 -0400

    urls and templates fixed

--- login/forms.py
+++ login/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
  
- class LoginForm(forms.Form):
- 	username = forms.CharField(label = "Enter Your Username")
+class LoginForm(forms.Form):
+	username = forms.CharField(label = "Enter Your Username")
  	pw = forms.CharField (widget =forms.PasswordInput, label ="Enter Your Password")
--- login/views.py
+++ login/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import authenticate, login
 from django.views.generic import CreateView, TemplateView
 from django.http import HttpResponseRedirect
+from forms import LoginForm
 
 
 
-class home(Templateview):
+class home(TemplateView):
 	 template_name = 'home'
 
 class siteLogin(CreateView):
@@ -12,7 +13,7 @@ class siteLogin(CreateView):
 	template_name = 'login.html'
 	success_url = ""
 	
-	def login-validation(self,form)
+	def login_validation(self,form):
 		username = request.POST['username']
 		pw = request.POST['password']
 		user = authentication(username=username, password=password)
@@ -23,6 +24,7 @@ class siteLogin(CreateView):
 			else:
 				return self.render_to_response(self.get_context_data(form=form))
 			messages.add_message(self.request, messages.INFO, message="Username or password does not exist")
-	    else:
-			return self.render_to_response(self.get_context_data(form=form)) messages.add_message(self.request, messages.INFO, message="Please enter a valid username and password")
+		else:
+			return self.render_to_response(self.get_context_data(form=form)) 
+			messages.add_message(self.request, messages.INFO, message="Please enter a valid username and password")
 			
