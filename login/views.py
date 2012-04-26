29dcdbb8962bd2c7c878ff27f7ed92b1648f5b95
Tamara <tamaramakarem@gmail.com>
2012-04-26 19:18:40 -0400

    login updates

--- login/views.py
+++ login/views.py
@@ -2,16 +2,32 @@ from django.contrib.auth import authenticate, login
 from django.views.generic import CreateView, TemplateView
 from django.http import HttpResponseRedirect
 from forms import LoginForm
-
+from django.contrib import messages
+from django.shortcuts import redirect
 
 
 class HomeView(TemplateView):
 	 template_name = 'home.html'
 
-class SiteLogin(CreateView):
+class SiteLogin(TemplateView):
 	form_class = LoginForm
-	template_name = 'login.html'
+	template_name = 'login/login.html'
 	success_url = ""
+	
+	def get_context_data(self, **kwargs):
+		self.form_class()
+		return {
+            'form': LoginForm() if not kwargs.get('form') else kwargs['form'],
+		}
+		
+	def post(self, request, *args, **kwargs):
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			messages.add_message(self.request, messages.INFO, message="You have succesfully logged in")
+			return redirect('')
+		else:
+			return self.get(request, form=LoginForm)
+
 	
 	def login_validation(self,form):
 		username = request.POST['username']
--- voodo/urls.py
+++ voodo/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls.defaults import patterns, url, include
+from django.contrib import admin
 
 from login.views import HomeView
 
